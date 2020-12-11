ee1ad289dd74911be2949085f60819bbbb8ce9fc
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-11 19:31:28 +0200

    implemented setImage for SquareView

--- ChessApp/View/SquareView.swift
+++ ChessApp/View/SquareView.swift
@@ -11,10 +11,23 @@ import UIKit
 class SquareView: UIView {
 
     private var square: Square?
+    private var imageView: UIImageView?
         
     func setSquare(square: Square) {
         self.square = square
         self.backgroundColor = (square.coordinates.reduce(0, +) % 2 == 0) ? UIColor.white : UIColor.gray
+        
+        if !self.square!.isEmpty {
+            self.setImage(named: self.square!.piece!.imageName)
+        }
+    }
+    
+    private func setImage(named imageName: String) {
+        let image = UIImage(named: imageName)
+        self.imageView = UIImageView(image: image!)
+        self.imageView!.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: self.frame.width, height: self.frame.height)
+        self.addSubview(self.imageView!)
+        self.bringSubviewToFront(self.imageView!)
     }
 
 }

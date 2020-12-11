68075191a2f2dcf3523cdbdc8993531cb496fe53
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-11 18:41:25 +0200

    modified drawSquares in BoardView

--- ChessApp/View/BoardView.swift
+++ ChessApp/View/BoardView.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class BoardView: UIView {
+    
+    private var board: Board?
 
     override func draw(_ rect: CGRect) {
         super.draw(rect)
@@ -16,19 +18,23 @@ class BoardView: UIView {
     }
     
     func drawSquares() {
+        guard let _ = self.board else { return }
+        
         let squareSide = self.frame.height / 8
         
-        for row in 0...Board.MAXROW {
-            var squareColor = (row % 2 == 0) ? UIColor.white : UIColor.gray
-            for col in 0...Board.MAXROW {
-                let squareRect = CGRect(x: squareSide * CGFloat(col), y: squareSide * CGFloat(row), width: squareSide, height: squareSide)
+        for row in self.board!.squares {
+            for square in row {
+                let squareRect = CGRect(x: squareSide * CGFloat(square.col), y: squareSide * CGFloat(square.row), width: squareSide, height: squareSide)
                 let squareRectView = UIView(frame: squareRect)
-                squareRectView.backgroundColor = squareColor
+                squareRectView.backgroundColor = square.color
                 self.addSubview(squareRectView)
-                
-                squareColor = (squareColor == UIColor.gray) ? UIColor.white : UIColor.gray
             }
         }
+        
+    }
+    
+    func setBoard(board: Board) {
+        self.board = board
     }
 
 }

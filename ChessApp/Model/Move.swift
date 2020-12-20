65a5ae400fc7cb4afca456156b16fd7aa8a1dbe7
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-20 22:23:41 +0200

    implemented a new struct Move for better representation and interaction with moves

--- ChessApp/Model/Move.swift
+++ ChessApp/Model/Move.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+
+
+struct Move {
+    
+    let from: Coordinates
+    let to: Coordinates
+    
+    var valid: Bool = false
+    var moveInvalidation: MoveInvalidation?
+    
+    var isDiagonal: Bool {
+        return from.col != to.col && from.row != to.row
+    }
+    
+    var line: [Coordinates] {
+        var coordinates: [Coordinates] = []
+        
+        let rowDiff = from.row - to.row
+        let colDiff = from.col - to.col
+                
+        var rowRange = rowDiff == 0 ? Array(repeating: from.row, count: abs(colDiff)) : Array([from.row, to.row].min()!..<[from.row, to.row].max()!)
+        rowRange.removeFirst()
+        if rowDiff > 0 {
+            rowRange.reverse()
+        }
+        
+        var colRange = colDiff == 0 ? Array(repeating: from.col, count: abs(rowDiff)) : Array([from.col, to.col].min()!..<[from.col, to.col].max()!)
+        colRange.removeFirst()
+        if colDiff > 0 {
+            colRange.reverse()
+        }
+        
+        for (row, col) in zip(rowRange, colRange) {
+            coordinates.append(Coordinates(row: row, col: col))
+        }
+        
+        return coordinates
+    }
+    
+    mutating func setInvalid(reason: MoveInvalidation) {
+        print(reason)
+        self.valid = false
+        self.moveInvalidation = reason
+    }
+    
+    
+}
+
+
+extension Move {
+    
+    enum MoveInvalidation {
+        case impossibleMove
+        case toOwnPiece
+        case putsOwnKingUnderCheck
+    }
+    
+}
